feb28ffbc51d3fdaf817a399dea848a5cac3eea0
eli22443 <eli22443@gmail.com>
2025-01-20 17:36:34 +0000

    server

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,26 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.api.StompMessagingProtocol;
+import bgu.spl.net.impl.echo.EchoProtocol;
+import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
+import bgu.spl.net.srv.Server;
+
 public class StompServer {
 
     public static void main(String[] args) {
         // TODO: implement this
+                Server.threadPerClient(
+                7777, //port
+                () -> new TCPStompMessegingProtocol<>(), //protocol factory
+                LineMessageEncoderDecoder::new //message encoder decoder factory
+        ).serve();
+
+        // Server.reactor(
+        //         Runtime.getRuntime().availableProcessors(),
+        //         7777, //port
+        //         () -> new EchoProtocol<>(), //protocol factory
+        //         LineMessageEncoderDecoder::new //message encoder decoder factory
+        // ).serve();
+
     }
 }
