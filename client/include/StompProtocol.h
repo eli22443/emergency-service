eb1a05edb4bf511e39679a5352e5fe906aa81ea6
eli22443 <eli22443@gmail.com>
2025-05-13 09:20:11 +0000

    filtered

--- client/include/StompProtocol.h
+++ client/include/StompProtocol.h
@@ -6,41 +6,42 @@
 #include <vector>
 #include <atomic>
 #include <iostream>
-#include <map>
+#include <>
 #include <string>
 #include <sstream>
 #include <vector>
 #include <thread>
 #include <fstream>
 #include <algorithm>
-#include "ConnectionHandler.h"  // Include your ConnectionHandler header here
-#include "event.h"              // Include your Event header here
+#include "ConnectionHandler.h" // Include your ConnectionHandler header here
+#include "event.h"             // Include your Event header here
 
-class StompProtocol {
+class StompProtocol
+{
 private:
     std::atomic<bool> shouldTerminate;
     std::atomic<bool> isConnected;
     std::atomic<int> disconnectReceiptId;
     std::atomic<int> subId;
     std::atomic<int> receiptId;
-    ConnectionHandler* connectionHandler;
+    ConnectionHandler *connectionHandler;
     std::string currentUser;
-    std::map<std::string, int> subscriptionIds;  
-    std::map<std::string, std::map<std::string, std::vector<Event>>> userEvents; 
-    
+    std::map<std::string, int> subscriptionIds;
+    std::map<std::string, std::map<std::string, std::vector<Event>>> userEvents;
+
 public:
     std::string getNextMessageId();
     bool isLoggedIn() const;
-    void processCommand(const std::string& command);
-    void handleLogin(const std::string& command);
-    void handleJoin(const std::string& command);
-    void handleExit(const std::string& command);
-    void handleReport(const std::string& command);
-    std::string createReportFrame(const Event& event, const std::string& channel);
-    void handleSummary(const std::string& command);
-    void writeSummary(std::ofstream& out, const std::string& channel, const std::vector<Event>& events);
+    void processCommand(const std::string &command);
+    void handleLogin(const std::string &command);
+    void handleJoin(const std::string &command);
+    void handleExit(const std::string &command);
+    void handleReport(const std::string &command);
+    std::string createReportFrame(const Event &event, const std::string &channel);
+    void handleSummary(const std::string &command);
+    void writeSummary(std::ofstream &out, const std::string &channel, const std::vector<Event> &events);
     void handleLogout();
-    void processServerMessage(const std::string& message);
+    void processServerMessage(const std::string &message);
     void dissconnect();
     std::string epoch_to_date(std::time_t timestamp);
     StompProtocol();
